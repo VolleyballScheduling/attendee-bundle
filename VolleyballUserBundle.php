370970e8a140c444a97734ac5f597515d4a56fd6
david durost <david.durost@gmail.com>
2014-09-21 12:48:29 -0400

    update to work with SyliusResourceBundle

--- VolleyballUserBundle.php
+++ VolleyballUserBundle.php
@@ -1,9 +1,35 @@
 <?php
-
 namespace Volleyball\Bundle\UserBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use \Symfony\Component\DependencyInjection\ContainerBuilder;
+use \Sylius\Bundle\ResourceBundle\AbstractResourceBundle;
+use \Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
 
-class VolleyballUserBundle extends Bundle
+class VolleyballUserBundle extends AbstractResourceBundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSupportedDrivers()
+    {
+        return array(
+            SyliusResourceBundle::DRIVER_DOCTRINE_ORM
+        );
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    protected function getBundlePrefix()
+    {
+        return 'volleyball_user';
+    }
 }
