aff73b0cd63721bed3f44b790bf9d9f6ca44fabb
david durost <david.durost@gmail.com>
2015-02-07 20:38:24 -0500

    initial attendee bundle split

--- Repository/AdminRepository.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Volleyball\Bundle\UserBundle\Repository;
-
-class AdminRepository extends \Volleyball\Bundle\UtilityBundle\Doctrine\ORM\EntityRepository
-{
-}
--- Repository/UserRepository.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Volleyball\Bundle\UserBundle\Repository;
-
-class UserRepository extends \Volleyball\Bundle\UtilityBundle\Doctrine\ORM\EntityRepository
-{
-}
